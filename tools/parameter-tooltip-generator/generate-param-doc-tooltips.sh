293261f8d6da8bf2d2fdbf47757d17fba294ed57
Slider0007 <jobbelle@gmx.net>
2023-06-22 19:40:54 +0200

    Update script

--- tools/parameter-tooltip-generator/generate-param-doc-tooltips.sh
+++ tools/parameter-tooltip-generator/generate-param-doc-tooltips.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 # Checkout the documentation repo branch which hosts the parameter description (tooltip on configuration page)
-if [ -d "AI-on-the-edge-device-docs" ] ; then
-    # Repo already checked out, pull it
-    cd AI-on-the-edge-device-docs
-    git checkout parameter-description
-    git pull
-    cd ..
-else
-    # Repos folde ris missing, clone it
-    git clone https://github.com/Slider0007/AI-on-the-edge-device-docs.git
-fi
+git clone https://github.com/Slider0007/AI-on-the-edge-device-docs.git
+git checkout parameter-description
+git pull
 
 python generate-param-doc-tooltips.py
