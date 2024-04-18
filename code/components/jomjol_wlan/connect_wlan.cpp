fac86fa02d360b68534f5d3c771180c5504fd77f
Slider0007 <jobbelle@gmx.net>
2024-04-18 21:10:56 +0200

    Update

--- code/components/jomjol_wlan/connect_wlan.cpp
+++ code/components/jomjol_wlan/connect_wlan.cpp
@@ -533,12 +533,23 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
         WIFIConnected = true;
 		WIFIReconnectCnt = 0;
 
-		char buf[20];
-		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-		wlan_config.ipaddress = std::string(esp_ip4addr_ntoa(&event->ip_info.ip, buf, sizeof(buf)));
-		wlan_config.netmask = std::string(esp_ip4addr_ntoa(&event->ip_info.netmask, buf, sizeof(buf)));
-		wlan_config.gateway = std::string(esp_ip4addr_ntoa(&event->ip_info.gw, buf, sizeof(buf)));
-		LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Assigned IP: " + wlan_config.ipaddress);
+		if (wlan_config.dhcp) {
+			char buf[20];
+			ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+
+			wlan_config.ipaddress = std::string(esp_ip4addr_ntoa(&event->ip_info.ip, buf, sizeof(buf)));
+			wlan_config.netmask = std::string(esp_ip4addr_ntoa(&event->ip_info.netmask, buf, sizeof(buf)));
+			wlan_config.gateway = std::string(esp_ip4addr_ntoa(&event->ip_info.gw, buf, sizeof(buf)));
+
+			esp_netif_dns_info_t dns_info;
+			esp_netif_get_dns_info(event->esp_netif, ESP_NETIF_DNS_MAIN, &dns_info);
+			wlan_config.dns = std::string(esp_ip4addr_ntoa((const esp_ip4_addr_t*)&dns_info.ip, buf, sizeof(buf)));
+
+			LogFile.WriteToFile(ESP_LOG_INFO, TAG, "DHCP | IP: " + wlan_config.ipaddress + 
+														", Netmask: " + wlan_config.netmask + 
+														", Gateway: " + wlan_config.gateway +
+														", DNS: " + wlan_config.dns); 
+		}
 
 		#ifdef ENABLE_MQTT
             if (getMQTTisEnabled()) {
@@ -565,9 +576,6 @@ esp_err_t wifi_init_sta(void)
 	}
 	
     my_sta = esp_netif_create_default_wifi_sta();
-
-    esp_netif_dns_info_t dns_info;
-	char buf[20];
 	
 	if (!wlan_config.ipaddress.empty() && !wlan_config.netmask.empty() && !wlan_config.gateway.empty())
     {	
@@ -594,7 +602,8 @@ esp_err_t wifi_init_sta(void)
 			LogFile.WriteToFile(ESP_LOG_INFO, TAG, "No DNS address set, use gateway address as DNS");
 			 wlan_config.dns = wlan_config.gateway;
 		}
-     
+
+		esp_netif_dns_info_t dns_info;
         dns_info.ip.u_addr.ip4.addr = esp_ip4addr_aton(wlan_config.dns.c_str());
 
         retval = esp_netif_set_dns_info(my_sta, ESP_NETIF_DNS_MAIN, &dns_info);
@@ -603,17 +612,14 @@ esp_err_t wifi_init_sta(void)
 			return retval;
 		}
 
-		wlan_config.dns = std::string(esp_ip4addr_ntoa((const esp_ip4_addr_t*)&dns_info.ip, buf, sizeof(buf)));
-		LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Manual interface config | IP: " + wlan_config.ipaddress + 
+		LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Static | IP: " + wlan_config.ipaddress + 
 													", Netmask: " + wlan_config.netmask + 
 													", Gateway: " + wlan_config.gateway +
 													", DNS: " + wlan_config.dns); 
     }
 	else {
 		wlan_config.dhcp = true;
-		wlan_config.dns = std::string(esp_ip4addr_ntoa((const esp_ip4_addr_t*)&dns_info.ip, buf, sizeof(buf)));
-		
-		LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Automatic interface config | Use DHCP service");
+		LogFile.WriteToFile(ESP_LOG_INFO, TAG, "Use DHCP");
 	}
 
 	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
