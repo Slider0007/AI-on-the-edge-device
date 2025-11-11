96a58cf3d1dc6da9d5ed4ab2d8a912ad233d64f3
Slider0007 <115730895+Slider0007@users.noreply.github.com>
2025-11-11 20:58:01 +0100

    chore(model): Update dig-class100 tflite model to v1.82 (#319)

--- code/test/components/config_handling/test_configClass.cpp
+++ code/test/components/config_handling/test_configClass.cpp
@@ -39,7 +39,7 @@ void test_configJsonParseAndSerialization()
         "source\",\"rawimagesretention\":3}},\"imagealignment\":{\"alignmentalgo\":0,\"searchfield\":{\"x\":20,\"y\":20},"
         "\"imagerotation\":\"0.0\",\"marker\":[{\"x\":1,\"y\":1},{\"x\":1,\"y\":1}],\"debug\":{\"savedebuginfo\":false}},"
         "\"numbersequences\":{\"sequence\":[{\"sequenceid\":0,\"sequencename\":\"main\"}]},\"digit\":{\"enabled\":true,\"model\":\"dig-"
-        "class100_0180_s2_q.tflite\",\"cnngoodthreshold\":\"0.50\",\"sequence\":[{\"sequenceid\":0,\"sequencename\":\"main\",\"roi\":[]}],"
+        "class100_0182_s2_q.tflite\",\"cnngoodthreshold\":\"0.50\",\"sequence\":[{\"sequenceid\":0,\"sequencename\":\"main\",\"roi\":[]}],"
         "\"debug\":{\"saveroiimages\":false,\"roiimageslocation\":\"/log/"
         "digit\",\"roiimagesretention\":3,\"roisavingsize\":0}},\"analog\":{\"enabled\":true,\"model\":\"ana-class100_0201_s1_q.tflite\","
         "\"sequence\":[{"
