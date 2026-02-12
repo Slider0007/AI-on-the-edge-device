314936c6ecc4ea1ef2e1796e1a8fd9752a3aa14f
Slider0007 <115730895+Slider0007@users.noreply.github.com>
2026-02-12 09:42:49 +0100

    Resolve review comment

--- code/components/image_handling/CImage.cpp
+++ code/components/image_handling/CImage.cpp
@@ -154,7 +154,7 @@ CImage &CImage::operator=(const CImage &other)
 
     // Allocate only if current buffer is too small
     if (allocatedSize < other.imgDataSize || externalMemory) {
-        uint8_t *newImgData = (uint8_t *)malloc_psram_heap(std::string(TAG) + "->Copy-assign (" + other.name + ")", other.imgDataSize,
+        uint8_t *newImgData = (uint8_t *)malloc_psram_heap(std::string(TAG) + "->Copy-assign (" + other.name + ")", other.allocatedSize,
                                                            MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
 
         if (!newImgData) {
